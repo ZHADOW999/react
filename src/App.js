9ba7665a6c67889ed5ade15e158cd5243c6bb1ef
Akashii0 <abdulkid151@gmail.com>
2024-11-18 22:26:58 +0100

    we did it 👌👌👌👌

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import axios from "axios";
+import { useState } from "react";
+
 function App() {
+const[data, setData] = useState('')
+const getQuote= ()=>{
+  axios.get('http://127.0.0.1:8000')
+  .then(res =>{
+    console.log(res.data.message)
+    setData(res.data.message)
+
+  }).catch(err=>{
+      //console.log(err)
+      window.alert(err)
+  })
+} 
+
   return (
-    <div>hi</div>
+    
+    <div>
+      <button onClick={getQuote}>get</button>
+      {data ? <p>{data}</p>:""}
+    </div>
   );
 }
 
