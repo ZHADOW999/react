192301255ca97a56caa702359444ef2f25fb3e6e
ZHADOW999 <babalolav9@gmail.com>
2024-11-20 17:35:38 +0100

    new updates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 function App() {
 const[data, setData] = useState('')
+const[post, setPost] = useState('')
 const getQuote= ()=>{
   axios.get('http://127.0.0.1:8000')
   .then(res =>{
@@ -11,15 +12,40 @@ const getQuote= ()=>{
 
   }).catch(err=>{
       //console.log(err)
-      window.alert(err)
+      window.alert("could not get server")
   })
 } 
+const handleSunbmit = (e)=>{
+  e.preventDefault()
+  axios.post('http://127.0.0.1:8000', {
+    
+    "content": post
+    })
+    .then(res =>{
+      console.log(res.data.message)
+    }).catch(err=>{
+      window.alert(err)
+    })
+}
+
 
   return (
     
     <div>
       <button onClick={getQuote}>get</button>
       {data ? <p>{data}</p>:""}
+      <form className="form" onSubmit={handleSunbmit}>
+        <label for="form">post:</label>
+        <input
+        id="form"
+        type="text"
+        required
+        value={post}
+        onChange={(e)=>setPost(e.target.value)}
+        ></input>
+        <p>{post}</p>
+        <button>post</button>
+      </form>
     </div>
   );
 }
